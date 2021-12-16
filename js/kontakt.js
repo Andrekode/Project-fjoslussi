a4c0565ccce4e7610b38a22bdedfa68282877c99
KodeRenaldas <95746005+KodeRenaldas@users.noreply.github.com>
2021-12-16 09:58:37 +0100

    PHP
    form ikke overwrite data lengre, og etter submit redirect to index.html, la til comments til php og js

--- js/kontakt.js
+++ js/kontakt.js
@@ -1,10 +1,11 @@
+//Constants
 const submitBtn = document.getElementById("submit")
 const navnInput = document.getElementById("navn")
 const telefonInput = document.getElementById("telefon")
 const emailInput = document.getElementById("email")
 const emneInput = document.getElementById("emne")
 const beskjedInput = document.getElementById("beskjed")
-
+//Function to clear existing inputs
 function submitFormClear () {
     navnInput.innerHTML = ""
     navnInput.innerHTML = ""
@@ -13,6 +14,7 @@ function submitFormClear () {
     emneInput.innerHTML = ""
     beskjedInput.innerHTML = ""
 }
+//Function to check if all inputs are filled, if not gives alert that the form is not filled
 function submitForm () {
     if (navnInput.value != "" & telefonInput.value != "" & emailInput.value != "" & emneInput.value != "" & beskjedInput.value != "") {
         submitBtn.disabled = false
